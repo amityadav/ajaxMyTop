acbb7e0db2a6b1dda1cc6e7d5bcaff56b8b1affb
AMIT <amit.yadav19@gmail.com>
2016-07-12 01:05:35 -0400

    Ajax Tooltip

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$dbServer = "mysql";
+$dbHost = "lighthouse.ptc.com";
+$dbUser = "root";
+$dbPass = "watErmill5";
+
+$filters = array("Id","User","Host","db","Command","Time","State","Info");
+?>
